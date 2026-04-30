162680a013bead0f4d0106d805f8011dac1b26e3
Yusuf Karakurt <yusufkarakurtt@outlook.com>
2026-04-30 17:08:54 +0300

    added docker setup, kafka producer, and step 1-2 notebooks

---
+++ producer/producer.py
@@ -0,0 +1,46 @@
+import time
+import json
+import random
+from datetime import datetime
+from kafka import KafkaProducer
+import pandas as pd
+
+print("Sistem başlatılıyor, Lütfen bekleyin...")
+time.sleep(20)
+
+producer = KafkaProducer(
+    bootstrap_servers='kafka:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+csv_path = '/data/dataset.csv'
+
+try:
+    print(f"Veriler okunuyor: {csv_path}")
+    df = pd.read_csv(csv_path)
+except FileNotFoundError:
+    print("Data klasöründe dataset.csv bulunamadı! Lütfen dosyayı ekleyin.")
+
+print("Kafka'ya veri aktarımı başlıyor...")
+
+msg_count = 0
+
+for index, row in df.iterrows():
+    message = {
+        "timestamp": str(datetime.now()),
+        "user_id": random.randint(1000, 9999), 
+        "event_type": random.choice(["click", "view", "purchase"]),
+        "related_id": random.randint(100, 999),
+        "data": row.to_dict()
+    }
+    
+    producer.send('project-topic', message)
+    msg_count += 1
+    
+    if msg_count % 50 == 0:
+        print(f"{msg_count} adet mesaj Kafka'ya başarıyla iletildi.")
+    
+    sleep_duration = random.uniform(0.01, 0.1)
+    time.sleep(sleep_duration)
+
+print(f"İşlem tamamlandı. Toplam {msg_count} mesaj Kafka'ya gönderildi.")
